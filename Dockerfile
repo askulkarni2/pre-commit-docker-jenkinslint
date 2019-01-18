15cb41e29d08e723c2f3057db15889dec51d9532
Apoorva Kulkarni <apoorva_kulkarni@intuit.com>
2019-01-17 16:27:39 -0800

    add some more plugins

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,12 @@ RUN curl -sL https://deb.nodesource.com/setup_11.x | bash - && \
 
 RUN npm install jflint -g
 
-RUN /usr/local/bin/install-plugins.sh pipeline-model-definition
-
+RUN /usr/local/bin/install-plugins.sh \
+    pipeline-model-definition \
+    performance \
+    gatling \
+    kubernetes \
+    cloudbees-folder \
+    blueocean \
+    ansicolor
 ADD run-jflint.sh /usr/local/bin/run-jflint.sh
--- run-jflint.sh
+++ run-jflint.sh
@@ -3,11 +3,8 @@
 export JENKINS_HOME=/tmp
 export COPY_REFERENCE_FILE_LOG=/tmp/copy_reference_file.log 
 
-echo $JENKINS_HOME
-echo $COPY_REFERENCE_FILE_LOG
-
-#/sbin/tini -s -- /usr/local/bin/jenkins.sh >/dev/null 2>&1 &
-#sleep 45
+/sbin/tini -s -- /usr/local/bin/jenkins.sh >/dev/null 2>&1 &
+sleep 45
 jflint -j http://localhost:8080 \
     -u admin \
     -p $(cat $JENKINS_HOME/secrets/initialAdminPassword) \
